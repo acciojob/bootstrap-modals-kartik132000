a8dfee413c072ff9dc346c33214de8c8839e45c0
kartik132000 <sujeetkumarsen37@gmail.com>
2025-01-31 20:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your code here
+ document.getElementById("open-modal").addEventListener("click", function() {
+            var myModal = new bootstrap.Modal(document.getElementById("newton-modal"));
+            myModal.show();
+        });
